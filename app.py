e701212f0c4be142448fc47775ab26286da0297b
Kaushik Ganguly <37682060+kwasganguly@users.noreply.github.com>
2024-10-29 04:05:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ button = st.button("Generate Reply")
 
 def generate_reply(review):
     response = openai.Completion.create(
-    model="text-davinci-002",
+    model="gpt-3.5-turbo-instruct",
     prompt=f"This is a restaurant review replier bot. This restaurant has been launched recently and there might be instances where customers might come up with bad reviews and express disappointment. You have to understand customer sentiment and assure him to provide best services during next visit. If the customer has any concerns address them.Also let him know the address of any one of the offices in Kolkata so that he could get his query resolved if needed.\n\nReview:{review}\n\nreplay:",
     temperature=0.9,
     max_tokens=100,
